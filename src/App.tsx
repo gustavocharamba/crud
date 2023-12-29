d624206b70638001bcf402157a912b6bf32b8935
Gustavo Charamba <gucharamba@gmail.com>
2023-12-29 19:20:27 -0300

    added global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Routes from "./routes/routes"
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './global/globalStyle';
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle/>
       <Routes/>
     </BrowserRouter>
   );
